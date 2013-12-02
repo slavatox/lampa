fb2e0389966b0d146694558b6ff58772152dcc11
tox <tox@PC-1>
2013-12-02 17:47:38 +0600

    Кнопки-шнопки+ текст+версия+препод

--- lampa/app/js/controllers.js
+++ lampa/app/js/controllers.js
@@ -6,6 +6,14 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', [function($scope) {
+   $scope.phones = [
+    {'name': 'Шамиль',
+     'family': 'Минасов'},
+    {'name': 'Светлана',
+     'family': 'Милушкина'},
+    {'name': 'Сагит™',
+     'family': 'Валеев'}
+  ];       
 
   }]);
