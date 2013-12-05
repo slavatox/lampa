164a7b345eb491301178fdc283bc13feab1a8017
tox <tox@PC-1>
2013-12-05 18:15:58 +0600

    Менюшка справа

--- lampa/app/js/controllers.js
+++ lampa/app/js/controllers.js
@@ -6,14 +6,16 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
-  .controller('MyCtrl2', [function($scope) {
+  .controller('MyCtrl2', ['$scope', function($scope) {
+          $scope.test= 'всем привет';
+          //alert('jjoo');
+          
    $scope.phones = [
     {'name': 'Шамиль',
      'family': 'Минасов'},
     {'name': 'Светлана',
      'family': 'Милушкина'},
-    {'name': 'Сагит™',
+    {'name': 'Сагит',
      'family': 'Валеев'}
-  ];       
-
+  ];     
   }]);
