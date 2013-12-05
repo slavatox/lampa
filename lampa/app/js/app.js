17586acbbbd1c8199b798ae14615f02edd7bc1ec
tox <tox@PC-1>
2013-12-05 21:45:57 +0600

    "Включил" меню слева

--- lampa/app/js/app.js
+++ lampa/app/js/app.js
@@ -12,11 +12,12 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/О кафедре', {templateUrl: 'partials/Ab_al.html', controller: 'MyCtrl1'});
   $routeProvider.when('/Бакалавриат', {templateUrl: 'partials/Bag_k.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/Контакты', {templateUrl: 'partials/Kont_al.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/Магистратура', {templateUrl: 'partials/Mg_s.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/Научная работа', {templateUrl: 'partials/Nayc_k.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/Специалитет', {templateUrl: 'partials/Spec_s.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/Преподаватели', {templateUrl: 'partials/Prep_s.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/Расписание', {templateUrl: 'partials/raspis_al.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/Контакты', {templateUrl: 'partials/Kont_al.html', controller: 'MyCtrl3'});
+  $routeProvider.when('/Магистратура', {templateUrl: 'partials/Mg_s.html', controller: 'MyCtrl4'});
+  $routeProvider.when('/Научная работа', {templateUrl: 'partials/Nayc_k.html', controller: 'MyCtrl5'});
+  $routeProvider.when('/Специалитет', {templateUrl: 'partials/Spec_s.html', controller: 'MyCtrl6'});
+  $routeProvider.when('/Преподаватели', {templateUrl: 'partials/Prep_s.html', controller: 'MyCtrl7'});
+  $routeProvider.when('/Расписание', {templateUrl: 'partials/raspis_al.html', controller: 'MyCtrl8'});
+  $routeProvider.when('/Домой', {templateUrl: 'partials/Novos.html', controller: 'MyCtrl9'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
