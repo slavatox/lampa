9ecb514dd135b7d4e51df1344be6ac0e3eb71a49
tox <tox@PC-1>
2013-12-06 23:42:46 +0600

    До сих пор манипуляции с меню. Убрал лишние кнопки сверху

--- lampa/app/js/app.js
+++ lampa/app/js/app.js
@@ -17,7 +17,8 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/Научная работа', {templateUrl: 'partials/Nayc_k.html', controller: 'MyCtrl5'});
   $routeProvider.when('/Специалитет', {templateUrl: 'partials/Spec_s.html', controller: 'MyCtrl6'});
   $routeProvider.when('/Преподаватели', {templateUrl: 'partials/Prep_s.html', controller: 'MyCtrl7'});
-  $routeProvider.when('/Расписание', {templateUrl: 'partials/raspis_al.html', controller: 'MyCtrl8'});
+  $routeProvider.when('/Расписание', {templateUrl: 'partials/Raspis_al.html', controller: 'MyCtrl8'});
   $routeProvider.when('/Домой', {templateUrl: 'partials/Novos.html', controller: 'MyCtrl9'});
+  $routeProvider.when('/Документы', {templateUrl: 'partials/Dock.html', controller: 'MyCtrl10'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
